c565f0966b7d91e7b3311f13087d2dcd1ada49e5
Sergey Kievskiy <knhel7@mail.ru>
2023-11-15 19:55:56 +0300

    step-3

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 #
+import os
+from pprint import pprint
+
 from django.apps import apps
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-from yaml import load as yaml_load, SafeLoader
+from yaml import load as yaml_load, SafeLoader, safe_load
 import requests
 
 from api.models import Store, Category, Product, ProductDetails, Parameter, ProductParameter
@@ -27,36 +30,58 @@ def get_user_models():
         )
 
 
-def upload_store_price(url=None, file_obj=None, user_id=0):
+def upload_store_price(url=None, file_name=None, file_obj=None, user_id=0, store_id=0):
     """
     Store price list update (file/url)
     """
-    if file_obj:
-        data = yaml_load(file_obj, Loader=SafeLoader)
-    else:
-        stream = requests.get(url).content
-        data = yaml_load(stream, Loader=SafeLoader)
+    result = 'SUCCESS'
+    if file_name:
+        path = os.path.join(os.getcwd())
+        file_route = os.path.join(path, "data", file_name)
+        try:
+            with open(file_route, encoding='utf-8') as f:
+                data = safe_load(f)
+        except OSError as err:
+            result = f'FAILURE:\n\t{err}'
+        print(f'reading... {result}!')  ##############
 
-    store, _ = Store.objects.get_or_create(name=data.get('store'), defaults={'owner_id': user_id})
+    # if file_obj:
+    #     data = yaml_load(file_obj, Loader=SafeLoader)
+    # else:
+    #     stream = requests.get(url).content
+    #     data = yaml_load(stream, Loader=SafeLoader)
 
+    pprint(data)
+
+    #######     Store
+    store, _ = Store.objects.get_or_create(
+        name=data.get('shop'),
+        defaults={'owner_id': user_id, 'store_id': store_id}
+    )
+
+    #######     Category
     for category in data['categories']:
-        category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
-        category_object.stores.add(store.id)
-        category_object.save()
+        category_obj, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+        category_obj.stores.add(store.id)
+        category_obj.save()
 
+    #######     ProductDetails
     ProductDetails.objects.filter(store_id=store.id).delete()
 
+    #######     Product
     for item in data['goods']:
-        product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+        product_obj, _ = Product.objects.get_or_create(name=item['name'])
+        product_obj.categories.add(item['category'])
+        product_obj.save()
 
-        product_detail = ProductDetails.objects.create(product_id=product.id,
-                                                       external_id=item['id'],
-                                                       price=item['price'],
-                                                       rr_price=item['price_rrc'],
-                                                       quantity=item['quantity'],
-                                                       store_id=store.id)
+        product_details = ProductDetails.objects.create(product_id=product_obj.id,
+                                                        external_id=item['id'],
+                                                        price=item['price'],
+                                                        rr_price=item['price_rrc'],
+                                                        quantity=item['quantity'],
+                                                        store_id=store.id)
         for name, value in item['parameters'].items():
-            parameter_object, _ = Parameter.objects.get_or_create(name=name)
-            ProductParameter.objects.create(product_detail_id=product_detail.id,
-                                            parameter_id=parameter_object.id,
+            parameter, _ = Parameter.objects.get_or_create(name=name)
+            ProductParameter.objects.create(product_detail_id=product_details.id,
+                                            parameter_id=parameter.id,
                                             value=value)
